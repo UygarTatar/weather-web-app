e30618a3281cb6b9f2dda72bbe9945f3afb06d3b
Uygartatar <uygartatar52@gmail.com>
2025-07-04 14:20:27 +0300

    redesigned welcome page

--- middleware/rateLimiters.js
+++ middleware/rateLimiters.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const usersLimiter = rateLimit ({
     windowMs: 10 * 60 * 1000,
-    max: 20,
+    max: 30,
     message: 'Too many requests. Please try again 10 minutes later.',
     standardHeaders: true,
     legacyHeaders: false
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,12 @@ const { ensureAuthenticated } = require('../config/auth');
 // Welcome page
 router.get('/', (req, res) => res.render('welcome', { page: 'welcome'}));
 
+// About page
+router.get('/about', (req, res) => res.render('about', { page: 'about', title: 'About' }));
+
+// Contact page
+router.get('/contact', (req, res) => res.render('contact', { page: 'contact', title: 'Contact' }));
+
 // Dashboard
 router.get('/dashboard', ensureAuthenticated, (req, res) => {
    res.render('dashboard', {
